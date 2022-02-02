d46ca2a2940634fca69a565740e5e21fa3df29d9
Kieran <g0041128@gmit.ie>
2022-02-02 15:54:31 +0000

    This is weekly task03

---
+++ Week3/secondstring.py
@@ -0,0 +1,12 @@
+# This is Weekly Task 03
+# The script will requets that a user enter a string
+# Every second letter of this string will be output in reverse
+# Author: Kieran Mullany
+# Version 1
+
+inputString = input("Please enter a string: ")
+#inputString.replace(" ","")
+print(inputString)
+print(inputString.replace(" ",""))
+print(inputString.replace(" ","") [::-2])
+
